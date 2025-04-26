ee577f6b854ef4e5ffa57791d462f265a24340a4
codeyash09 <112034820+codeyash09@users.noreply.github.com>
2025-04-25 19:11:53 -0700

    notes work
    woohoo. customization coming soon.

--- scripts/notes.js
+++ scripts/notes.js
@@ -3,8 +3,9 @@ let textArea = document.getElementById("text");
 let userIdent = window.localStorage.getItem('userIdentify');
 let noteName = document.getElementById("first");
 let submit = document.getElementById("saveButton");
+let noteList = document.getElementById("pageList");
 
-
+let currentNote;
 
 if(!localStorage.getItem('loggedInStatus')){
     window.location.replace('/pages/login.html');
@@ -31,9 +32,70 @@ async function newNotes(){
     ]);
 }
 
+async function updateNotes(id, updates) {
+    const { data, error } = await db
+      .from('Notes') 
+      .update(updates) 
+      .eq('noteId', id); 
+  
+    
+  }
+
 submit.addEventListener('click', () =>{
     if(noteName.innerHTML != '<span class="placeholder">Title...</span>' && textArea.innerHTML != '<span class="placeholder">Begin creating . . . <br><br><br><br><br></span>'){
-        newNotes();
+        if(currentNote == null){
+            newNotes();
+        }else{
+            updateNotes(currentNote, {
+                title: noteName.innerHTML,
+                notes: textArea.innerHTML,
+                date: new Date(),
+            });
+        }
     }
 
 });
+
+
+async function showNotes(){
+    const { data, error } = await db
+        .from('Notes') 
+        .select('*') 
+        .eq('userId', userIdent); 
+
+    const newest = data.reverse();
+
+    for(const note of newest){
+        if(note.title){
+            const title = document.createElement("a");
+            title.innerHTML = note.title;
+
+            title.addEventListener("click", () =>{
+                AdjustNote(note.noteId);
+            });
+            noteList.appendChild(title);
+        }
+    }
+}
+
+showNotes();
+
+
+async function AdjustNote(id){
+    const { data, error } = await db
+        .from('Notes') 
+        .select('*') 
+        .eq('noteId', id); 
+
+
+    for(const note of data){
+        if(note.title){
+            noteName.innerHTML = note.title;
+        }
+        if(note.notes){
+            textArea.innerHTML = note.notes;
+        }
+        
+    }
+    currentNote = id;
+}
