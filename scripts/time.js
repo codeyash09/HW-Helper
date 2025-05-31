8ba2d49e2a44a7a1b155558dbf3f1c362c7aa8e3
codeyash09 <112034820+codeyash09@users.noreply.github.com>
2025-05-31 10:13:59 -0700

    timestamp fixed
    timestamp

---
+++ scripts/time.js
@@ -0,0 +1,90 @@
+function convertToLocalTime(centralTime) {
+    // Parse the ISO timestamp
+    let date = new Date(centralTime);
+
+    let rn = new Date();
+
+    let daysApart = (rn - date)*0.001*(1/60)*(1/60)*(1/24);
+
+    let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let dayOfWeek = weekdays[date.getDay()];
+
+    if(daysApart > 6){
+        let options = { 
+             
+            year: 'numeric', 
+            month: 'long', 
+            day: 'numeric', 
+            hour: '2-digit', 
+            minute: '2-digit', 
+    
+        };
+
+        return date.toLocaleString(undefined, options);
+    }
+
+    if(daysApart <= 6){
+
+        if((rn.getDay() == date.getDay() + 1) || (rn.getDay() == 0 && date.getDay() == 6)){
+            let options = { 
+                
+                
+                hour: '2-digit', 
+                minute: '2-digit', 
+           
+            };
+
+            return "Yesterday at " + date.toLocaleString(undefined, options);
+        }
+
+        else if(rn.getDay() == date.getDay()){
+            let options = { 
+                
+                
+                hour: '2-digit', 
+                minute: '2-digit', 
+           
+            };
+
+            return "Today at " + date.toLocaleString(undefined, options);
+        }
+        else{
+            let options = { 
+                
+                weekday: "long",
+                hour: '2-digit', 
+                minute: '2-digit', 
+           
+            };
+
+            return date.toLocaleString(undefined, options);
+        }
+    }
+
+    
+
+    // Get the local time and formatted date
+    
+}
+refreshTime();
+function isValidTimestamp(inputString) {
+    return inputString.includes("T") && inputString.includes("+");
+}
+
+
+setInterval(refreshTime, 100);
+
+function refreshTime(){
+    
+    let timestamps = document.getElementsByClassName("timestamp");
+
+    for(let i = 0; i< timestamps.length; i++){
+        if(isValidTimestamp(timestamps[i].innerHTML)){
+            let converted = convertToLocalTime(timestamps[i].innerHTML);
+            timestamps[i].innerHTML = converted;
+        }
+        
+    }
+}
+
+
